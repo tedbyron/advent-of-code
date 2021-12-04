eca9e35b311549a3053c0d91638f44ae910c53c5
Teddy Byron <ted@tedbyron.com>
2021-12-04 18:12:11 -0500

    shorter day 3 solution

--- src/day_3/day_3.exs
+++ src/day_3/day_3.exs
@@ -4,8 +4,7 @@ defmodule Day3 do
   """
 
   def power_consumption_1(input) do
-    input
-    |> Stream.zip_with(& &1)
+    Stream.zip_with(input, & &1)
     |> Stream.map(fn col -> Enum.frequencies(col) |> Enum.max_by(&elem(&1, 1)) |> elem(0) end)
     |> Stream.map(fn
       0 -> {0, 1}
@@ -15,9 +14,7 @@ defmodule Day3 do
     |> then(fn {γ, ε} -> Integer.undigits(γ, 2) * Integer.undigits(ε, 2) end)
   end
 
-  def power_consumption_2(input) do
-    (recurse(input, &<=/2) |> Integer.undigits(2)) * (recurse(input, &>/2) |> Integer.undigits(2))
-  end
+  def power_consumption_2(input), do: recurse(input, &<=/2) * recurse(input, &>/2)
 
   defp recurse(input, fun, idx \\ 0) do
     Stream.map(input, &Enum.at(&1, idx))
@@ -25,7 +22,7 @@ defmodule Day3 do
     |> then(fn freqs -> (fun.(freqs[0], freqs[1]) && 0) || 1 end)
     |> then(fn min_max -> Enum.filter(input, &(Enum.at(&1, idx) == min_max)) end)
     |> then(fn
-      [output] -> output
+      [output] -> Integer.undigits(output, 2)
       output -> recurse(output, fun, idx + 1)
     end)
   end
@@ -38,5 +35,5 @@ input =
 
 IO.puts(Day3.power_consumption_1(input))
 # 2250414
-IO.inspect(Day3.power_consumption_2(input))
+IO.puts(Day3.power_consumption_2(input))
 # 6085575
