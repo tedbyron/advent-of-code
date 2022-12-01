5373261ec1d341928b56cd7c191241832b0cd73b
Teddy Byron <ted@tedbyron.com>
2022-12-01 11:38:21 -0500

    update project layout

--- 2022/src/bin/day_01.rs
+++ 2022/src/bin/day_01.rs
@@ -1,13 +1,12 @@
 #![warn(clippy::all, clippy::nursery, rust_2018_idioms)]
 
 fn main() {
-    let input = include_str!("day_1.txt");
-    let mut elves = input
+    let mut elves = include_str!("../inputs/day_01.txt")
         .split("\n\n")
         .map(|elf| elf.split('\n').flat_map(str::parse::<i32>).sum::<i32>())
         .collect::<Vec<_>>();
-    elves.sort_unstable_by(|a, b| b.cmp(a));
+    elves.select_nth_unstable_by(2, |a, b| b.cmp(a));
 
     println!("{}", elves[0]);
-    println!("{}", elves[0..3].iter().sum::<i32>());
+    println!("{}", elves[..3].iter().sum::<i32>());
 }
