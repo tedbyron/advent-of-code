1606e24970d7fe86c9aa94c3bf77483ad00ed041
Teddy Byron <ted@tedbyron.com>
2022-12-02 11:24:13 -0500

    update day 2

--- 2022/src/bin/day_02.rs
+++ 2022/src/bin/day_02.rs
@@ -2,12 +2,12 @@ fn main() {
     let mut p1 = 0;
     let mut p2 = 0;
 
-    for mut game in include_str!("../inputs/day_02.txt")
+    for mut chars in include_str!("../inputs/day_02.txt")
         .lines()
         .map(|line| line.chars().filter(char::is_ascii_alphabetic))
     {
-        let abc = game.next().unwrap() as u8 - b'A';
-        let xyz = game.next().unwrap() as u8 - b'X';
+        let abc = chars.next().unwrap() as u8 - b'A';
+        let xyz = chars.next().unwrap() as u8 - b'X';
         p1 += u32::from((xyz + 1) + ((xyz + 1 + (3 - abc)) % 3 * 3));
         p2 += u32::from(((xyz + abc + 2) % 3 + 1) + (xyz * 3));
     }
