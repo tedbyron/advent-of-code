4c2e92469ae31c7426457f6f5a4797ac11d648ce
Teddy Byron <ted@tedbyron.com>
2022-12-04 04:24:38 -0500

    day 4

---
+++ 2022/src/bin/day_04.rs
@@ -0,0 +1,25 @@
+fn main() -> anyhow::Result<()> {
+    let mut p1 = 0;
+    let mut p2 = 0;
+
+    for line in std::fs::read_to_string("inputs/day_04.txt")?.lines() {
+        let nums = line
+            .split([',', '-'])
+            .flat_map(str::parse::<u8>)
+            .collect::<Vec<_>>();
+
+        if (nums[0] >= nums[2] && nums[1] <= nums[3]) || (nums[2] >= nums[0] && nums[3] <= nums[1])
+        {
+            p1 += 1;
+        }
+
+        if nums[0] <= nums[3] && nums[2] <= nums[1] {
+            p2 += 1;
+        }
+    }
+
+    println!("{p1}");
+    println!("{p2}");
+
+    Ok(())
+}
