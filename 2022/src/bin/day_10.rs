50925176d530fa97e0d3f3bde7af81be6500281a
Teddy Byron <ted@tedbyron.com>
2022-12-20 17:36:25 -0500

    day 10 p1

--- 2022/src/bin/day_10.rs
+++ 2022/src/bin/day_10.rs
@@ -1,6 +1,42 @@
+struct P1 {
+    cycle_count: i32,
+    signal_strength: i32,
+    sum: i32,
+}
+
+impl P1 {
+    fn cycle(&mut self) {
+        self.cycle_count += 1;
+        if [20, 60, 100, 140, 180, 220].contains(&self.cycle_count) {
+            self.sum += self.cycle_count * self.signal_strength
+        }
+    }
+}
+
 fn main() -> anyhow::Result<()> {
     let input = std::fs::read_to_string("inputs/day_10.txt")?;
+    let mut p1 = P1 {
+        cycle_count: 0,
+        signal_strength: 1,
+        sum: 0,
+    };
 
-    println!("{}, {}", p1, p2);
+    for line in input
+        .lines()
+        .map(str::split_ascii_whitespace)
+        .map(Iterator::collect::<Vec<_>>)
+    {
+        match *line {
+            ["noop"] => p1.cycle(),
+            ["addx", v] => {
+                p1.cycle();
+                p1.cycle();
+                p1.signal_strength += v.parse::<i32>()?;
+            }
+            _ => unreachable!(),
+        }
+    }
+
+    println!("{}, {}", p1.sum, p2);
     Ok(())
 }
