d7619dae78a2f25a51c204fe7848cd53d01888a0
Teddy Byron <ted@ted.ooo>
2024-05-17 17:59:57 -0400

    Add day 10

---
+++ 2023/src/bin/day_10.rs
@@ -0,0 +1,14 @@
+fn main() {
+    let input = advent_of_code_2023::read_input!()
+        .lines()
+        .map(|l| l.chars().collect::<Box<_>>())
+        .collect::<Box<_>>();
+    let (start_i, start_j) = input
+        .iter()
+        .enumerate()
+        .filter_map(|(i, l)| l.iter().position(|c| c == &'S').map(|j| (i, j)))
+        .next()
+        .unwrap();
+
+    println!("{start_i}, {start_j}");
+}
